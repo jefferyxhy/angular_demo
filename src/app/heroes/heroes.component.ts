fd727b88647c40fcfcb8601fcb1b504931b0e141
Hongyi (Jeffery) Xie <jeffery.xie@pulse.com.au>
2019-07-15 11:35:59 +0930

    finish demo practice

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
-  constructor(private heroeservice: HeroService) {
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit() {
@@ -18,7 +18,21 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroeservice.getHeroes()
+    this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return; }
+    this.heroService.addHero({ name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
